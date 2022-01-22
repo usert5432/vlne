112dc54b78ec13475d0fd5c26aede700a71496ab
Dmitrii Torbunov <dtorbunov@bnl.gov>
2022-01-22 15:51:38 -0500

    scripts: Add a script to validate file preds

---
+++ scripts/validation/validate_file_preds.py
@@ -0,0 +1,113 @@
+import argparse
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from cafplot.plot          import save_fig
+from lstm_ee.consts        import LABEL_TOTAL, LABEL_PRIMARY
+from lstm_ee.data          import load_data
+from lstm_ee.eval.predict  import predict_energies
+from lstm_ee.utils.eval    import modify_concurrency_args
+from lstm_ee.utils.io      import load_model
+from lstm_ee.utils.log     import setup_logging
+from lstm_ee.utils.parsers import add_concurrency_parser
+
+LABELS = [ LABEL_PRIMARY, LABEL_TOTAL ]
+
+def parse_cmdargs():
+    parser = argparse.ArgumentParser(
+        "Compare model prediction to the ones stored in the file"
+    )
+
+    parser.add_argument(
+        'outdir',
+        help    = 'directory with saved model',
+        metavar = 'OUTDIR',
+        type    = str,
+    )
+
+    parser.add_argument(
+        '-d', '--data',
+        help    = 'evaluation dataset',
+        default = None,
+        dest    = 'data',
+        type    = str,
+    )
+
+    parser.add_argument(
+        '-p', '--plots',
+        help    = 'plot directory',
+        default = 'plots',
+        dest    = 'plotdir',
+        type    = str,
+    )
+
+    parser.add_argument(
+        '-e', '--ext',
+        help    = 'plot file extension. Example: pdf',
+        default = [ 'png' ],
+        dest    = 'ext',
+        nargs   = '+',
+        type    = str,
+    )
+
+    parser.add_argument(
+        '--preds',
+        help    = 'names of branches with predictions in the input file',
+        default = [ 'vlne.primaryE', 'vlne.totalE' ],
+        dest    = 'file_preds',
+        nargs   = 2,
+        type    = str,
+    )
+
+    add_concurrency_parser(parser)
+
+    return parser.parse_args()
+
+def make_comparison_plot(test_preds, null_preds, label):
+    f, ax = plt.subplots()
+    ax.set_yscale('log')
+
+    diff = 2 * (test_preds - null_preds) / (test_preds + null_preds)
+
+    diff[np.isnan(test_preds)] =  1
+    diff[np.isnan(null_preds)] = -1
+    diff[np.isnan(null_preds) & np.isnan(test_preds)] = 0
+
+    ax.hist(100 * diff, bins = 100)
+
+    ax.set_xlabel('(Test - Null) / Null [%]')
+    ax.set_ylabel('Events')
+    ax.set_title(label)
+
+    return f
+
+def main():
+    setup_logging()
+    cmdargs = parse_cmdargs()
+
+    args, model = load_model(cmdargs.outdir, compile = False)
+    modify_concurrency_args(args, cmdargs)
+
+    args.root_datadir     = ''
+    args.config.dataset   = cmdargs.data
+    args.config.test_size = None
+    args.config.weights   = None
+
+    args.config.var_target_primary = cmdargs.file_preds[0]
+    args.config.var_target_total   = cmdargs.file_preds[1]
+
+    dgen            = load_data(args)[0]
+    test_preds_dict = predict_energies(args, dgen, model)
+
+    for idx,label in enumerate(LABELS):
+        null_preds = dgen.data_loader.get(cmdargs.file_preds[idx])
+        test_preds = test_preds_dict[label]
+
+        f = make_comparison_plot(test_preds, null_preds, label)
+        save_fig(f, os.path.join(cmdargs.plotdir, label), cmdargs.ext)
+
+if __name__ == '__main__':
+    main()
+
