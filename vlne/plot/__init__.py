e3cafd30ecce3a2dbc4a37cc4257d07fb1a1785d
Dmitrii Torbunov <dtorbunov@bnl.gov>
2022-01-27 19:29:25 -0500

    plot: Use "boring" plot style by default

--- vlne/plot/__init__.py
+++ vlne/plot/__init__.py
@@ -4,11 +4,6 @@ This module contains functions to make various plots.
 Notes
 -----
 During the module initialization it sets the default plot parameters.
-It also recognizes presence of the environment variable `BORING_STYLE`.
-
-If `BORING_STYLE` environment variable is set, then the plots style will be
-adjusted to better suite article style.
-Otherwise, the plot style will be adjusted to look nice in presentations.
 """
 
 import os
@@ -18,14 +13,5 @@ import matplotlib as mpl
 
 from cycler import cycler
 
-if 'BORING_STYLE' in os.environ:
-    mpl.rcParams['axes.prop_cycle'] = cycler(color = 'krgbcmy')
-else:
-    mpl.rcParams['figure.figsize']   = [ 8.0, 6.0 ]
-    mpl.rcParams['figure.dpi']       = 80
-    mpl.rcParams['savefig.dpi']      = 100
-
-    mpl.rcParams['font.size']        = 10
-    mpl.rcParams['legend.fontsize']  = 'large'
-    mpl.rcParams['figure.titlesize'] = 'medium'
+mpl.rcParams['axes.prop_cycle'] = cycler(color = 'krgbcmy')
 
