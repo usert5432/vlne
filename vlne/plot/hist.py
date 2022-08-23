5a281ddace2216cc3d92a75a6456d9cf1081e639
Dmitrii Torbunov <dtorbunov@bnl.gov>
2022-08-23 15:45:08 -0400

    plot/hist: Refactor to improve code readability

--- vlne/plot/hist.py
+++ vlne/plot/hist.py
@@ -12,9 +12,10 @@ from cafplot.plot  import (
 )
 from cafplot.rhist import RHist1D
 
+from vlne.eval.funcs import get_weights
+
 def plot_hist_base(
-    list_of_data_weight_label_color, key, spec, ratio_plot_type, stat_err,
-    log = False
+    hist_data_list, target, spec, ratio_plot_type, stat_err, log = False
 ):
     """Plot multiple energy histograms"""
 
@@ -28,11 +29,13 @@ def plot_hist_base(
 
     list_of_rhist_color = []
 
-    for (data,weights,label,color) in list_of_data_weight_label_color:
-        rhist = RHist1D.from_data(data[key], spec.bins_x, weights)
+    for hist_data in hist_data_list:
+        values  = hist_data.values[target]
+        weights = get_weights(hist_data.weights, target, hist_data.values)
+        rhist   = RHist1D.from_data(values, spec.bins_x, weights)
 
         centers = (rhist.bins_x[1:] + rhist.bins_x[:-1]) / 2
-        mean = np.average(centers, weights = rhist.hist)
+        mean    = np.average(centers, weights = rhist.hist)
 
         plot_rhist1d(
             ax, rhist,
@@ -40,17 +43,17 @@ def plot_hist_base(
             marker    = None,
             linestyle = '-',
             linewidth = 2,
-            label     = "%s. MEAN = %.3e" % (label, mean),
-            color     = color,
+            label     = "%s. MEAN = %.3e" % (hist_data.label, mean),
+            color     = hist_data.color,
         )
 
         if stat_err:
             plot_rhist1d_error(
-                ax, rhist, err_type = 'bar', color = color, linewidth = 2,
-                alpha = 0.8
+                ax, rhist, err_type = 'bar', color = hist_data.color,
+                linewidth = 2, alpha = 0.8
             )
 
-        list_of_rhist_color.append((rhist, color))
+        list_of_rhist_color.append((rhist, hist_data.color))
 
     spec.decorate(ax, ratio_plot_type)
 
@@ -71,28 +74,16 @@ def plot_hist_base(
     return f, ax
 
 def plot_energy_hists(
-    list_of_data_weight_label_color, plot_specs, fname, ext, log = False
+    hist_data_list, plot_specs, fname, ext, log = False
 ):
     """Make and save plots of energy histograms.
 
     Parameters
     ----------
-    list_of_pred_true_weight_label_color : list
-        List of tuples of the form (pred, true, weights, label, color) where:
-        data : dict
-            Dictionary where keys are energy labels and values are the
-            `ndarray` (shape (N,)) of energies.
-        weights : `ndarray`, shape (N,)
-            Sample weights.
-        label : str
-            Plot label.
-        color : str
-            Line color.
-        A separate plot will be made for each key in the `data`.
-        Lines for all elements of the `list_of_data_weight_label_color`
-        will be drawn on each plot.
+    hist_data_list : list of HistData
+        List of hist data containers to plot.
     plot_specs : dict
-        Dictionary where keys are energy labels and values are `PlotSpec` that
+        Dictionary where targets are energy labels and values are `PlotSpec` that
         specify axes and bins of the energy plots.
     fname : str
         Prefix of the path that will be used to build plot file names.
@@ -104,17 +95,16 @@ def plot_energy_hists(
         Default: False.
     """
 
-    for k in plot_specs.keys():
+    for target in plot_specs.keys():
         for ratio_plot_type in [ None, 'auto', 'fixed' ]:
             for stat_err in [ True, False ]:
-
                 f, _ = plot_hist_base(
-                    list_of_data_weight_label_color, k, plot_specs[k],
+                    hist_data_list, target, plot_specs[target],
                     ratio_plot_type, stat_err, log
                 )
 
                 fullname = "%s_%s_ratio-%s_staterr-%s" % (
-                    fname, k, ratio_plot_type, stat_err
+                    fname, target, ratio_plot_type, stat_err
                 )
                 save_fig(f, fullname, ext)
                 plt.close(f)
