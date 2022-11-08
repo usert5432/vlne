216ab2ed3a28415bf8b10d9e68377012e329a87e
Dmitrii Torbunov <dtorbunov@bnl.gov>
2022-11-08 18:14:17 -0500

    keras: Move setup_objs to __init__
    Also register SimpleNorm in setup_objs

--- vlne/keras/__init__.py
+++ vlne/keras/__init__.py
@@ -2,6 +2,38 @@
 This module contains a collection of `keras` related objects.
 """
 
-from vlne.keras.setup_objs import setup_keras_custom_objects
+from tensorflow.keras.utils import get_custom_objects
+from .losses import (
+    cl_mean_relative_error, cl_ms_relative_error,
+    relative_error_huber, error_huber
+)
+
+from .models.layer_norm import SimpleNorm
+
+def setup_keras_custom_objects():
+    """Register custom `keras` losses"""
+    get_custom_objects().update({
+        "mean_relative_error"    : cl_mean_relative_error,
+        "ms_relative_error"      : cl_ms_relative_error,
+    })
+
+    get_custom_objects().update({
+        "relative_error_huber%.2f" % (delta) :
+            lambda x, y, delta = delta : relative_error_huber(x, y, delta)
+        for delta in [
+            0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 2, 5, 10
+        ]
+    })
+
+    get_custom_objects().update({
+        "error_huber%.2f" % (delta) :
+            lambda x, y, delta = delta : error_huber(x, y, delta)
+        for delta in [
+            0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 2, 5, 10
+        ]
+    })
+
+    get_custom_objects().update({ "SimpleNorm" : SimpleNorm })
 
 setup_keras_custom_objects()
+
